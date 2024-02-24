d0dba0abb924171b3376980e4c523ea53e966f8c
luoweigen <1193984561@qq.com>
2024-02-24 23:26:55 +0800

    提交

--- src/main/java/com/example/wenmanager/controller/UserController.java
+++ src/main/java/com/example/wenmanager/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.example.wenmanager.controller;
 
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONObject;
 import com.example.wenmanager.bean.User;
 import com.example.wenmanager.common.R;
 import com.example.wenmanager.service.UserService;
+import com.example.wenmanager.vo.WeChatVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +17,19 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Value("${wechat.config.AppID}")
+    private String appid;
+
+    @Value("${wechat.config.AppSecret}")
+    private String appSecret;
+
+    @PostMapping("/get_wechat_code")
+    public R getWechatCode(@RequestBody WeChatVo weChatVo){
+        String s = HttpUtil.get(String.format("https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code", appid, appSecret, weChatVo.getCode()));
+        JSONObject jsonObject = new JSONObject(s);
+        return R.SUCCESS(jsonObject);
+    }
 
     @PostMapping("/wechat_login")
     public R weChatLogin(@RequestBody User user){
---
+++ src/main/java/com/example/wenmanager/vo/WeChatVo.java
@@ -0,0 +1,8 @@
+package com.example.wenmanager.vo;
+
+import lombok.Data;
+
+@Data
+public class WeChatVo {
+    private String code;
+}
